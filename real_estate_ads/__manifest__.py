9ed661e35f3e421ca7ca2d61e54fbe60742c3b00
Francisco-Baez <152637296+francisco817@users.noreply.github.com>
2024-02-06 17:42:02 -0400

    Add files via upload

---
+++ real_estate_ads/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Real estate ads",
+    "description": """
+    This module
+    """,
+    "category": "sales",
+    "depends": [""],
+    "data": [],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+    "version": "1.0",
+    "author": "Francisco",
+    "website": "https://github.com",
+
+}
